ccb0d563f5f5c9b5fdf20c78d42d68d7a74b7e1e
Nicole Jelonek <nicolejelonek@gmail.com>
2018-09-24 18:22:20 +0200

    outfit still

--- server/routes/ootd.js
+++ server/routes/ootd.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.get("/ootd", isLoggedIn, (req, res, next) => {
   Item.find({
     _owner: req.user._id,
-    // wornOn: [new Date().toISOString().slice(0, 10)]
+    wornOn: [new Date().toISOString().slice(0, 10)]
     // wornOn: [new Date().toLocaleDateString()]
   })
     .then(items => {
@@ -30,6 +30,33 @@ router.post("/ootd", isLoggedIn, (req, res, next) => {
   ).then(item => {
     res.json(item)
   })
+})
+
+router.get("/ootd/addOutfit", isLoggedIn, (req, res, next) => {
+  Item.find({ _owner: req.user._id })
+    .then(items => {
+      res.json(items)
+    })
+    .catch(err => next(err))
+})
+
+router.post('/ootd/addOutfit', isLoggedIn, parser.single('picture'), (req, res, next) => {
+  let _owner = req.user._id
+  let pictureUrl = req.file.secure_url
+  let { _category, subcategory, season, color, tags, brand, boughtOn, price } = req.body
+  Item.create({ _category, subcategory, season, color, tags, brand, boughtOn, price, _owner, pictureUrl })
+    .then(item => {
+      res.json({
+        success: true,
+        item
+      });
+    })
+    .catch(err => {
+      res.json({
+        success: false,
+        error: err
+      })
+    })
 })
 
 module.exports = router
