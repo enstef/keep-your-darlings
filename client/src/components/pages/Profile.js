64036fec915f4871355bc1da3b61b1c83f616d2f
Nicole Jelonek <nicolejelonek@gmail.com>
2018-09-26 12:37:26 +0200

    css for favicon

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import api from '../../api'
-// import ItemCard from './ItemCard';
+import ItemCard from './ItemCard';
 
 class Profile extends Component {
   constructor(props) {
@@ -23,39 +23,40 @@ class Profile extends Component {
       "Darling, a beautiful thing is never perfect.",
       "Life is tough, my darling, but so are you."
     ]
-    let randomPhrase = phrases[Math.floor(Math.random()*phrases.length)]
+    let randomPhrase = phrases[Math.floor(Math.random() * phrases.length)]
     return (
       <div className="Profile onepage">
         <h1>{this.state.user.username}s closet</h1>
         <p className="random">{randomPhrase}</p>
-        {/* TODO 
+
         <p>Your top 3 Darlings</p>
-        {this.state.items.sort((a, b) => b.wornOn.length - a.wornOn.length).slice(0,3).map((item, i) => {
+        {this.state.items.sort((a, b) => b.wornOn.length - a.wornOn.length).slice(0, 3).map((item, i) => {
           <ItemCard item={item} key={i} />
         })}
         <p>Your least 3</p>
-        {this.state.items.sort((a, b) => a.wornOn.length - b.wornOn.length).slice(0,3).map((item, i) => {
+        {this.state.items.sort((a, b) => a.wornOn.length - b.wornOn.length).slice(0, 3).map((item, i) => {
           <ItemCard item={item} key={i} />
-        })} */}
-        <Link to="/ootd">OOTD</Link> <br/>
+        })}
+        <Link to="/ootd">OOTD</Link> <br />
         <Link to="/closet">Closet</Link>
+        <h1 class="kyd">KYD</h1>
       </div>
     );
   }
 
   componentDidMount() {
     api.getProfile()
-    .then(user => {
-      this.setState({
-        user: user
+      .then(user => {
+        this.setState({
+          user: user
+        })
       })
-    })
     api.getCloset()
-    .then(items => {
-      this.setState({
-        items: items
+      .then(items => {
+        this.setState({
+          items: items
+        })
       })
-    })
   }
 }
 
